c4d413557b8b7a1ad1ed26f405ed3c6fc1e7c25f
Kanatnikov <dmitry.kanatnikov@ligastavok.ru>
2022-06-05 20:45:04 +0300

    Test 2

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,13 @@
 public class MainClass {
-    public static int getLocalNumber (){
+
+
+    private int class_number = 20;
+
+    public int getClass_number() {
+        return class_number;
+    }
+
+    public static int getLocalNumber() {
         return 14;
     }
 }
--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -8,4 +8,10 @@ public class MainClassTest {
     public void testGetLocalNumber() {
         Assertions.assertEquals(14, MainClass.getLocalNumber(), "Ошибка: Метод 'getLocalNumber' вернул не '14'");
     }
+
+    @Test
+    public void  testGetClassNumber(){
+        MainClass mainClass = new MainClass();
+        Assertions.assertTrue(mainClass.getClass_number() >= 45, "Ошибка: getClass_number вернул число меньше 45");
+    }
 }
